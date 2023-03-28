a9c1b00ebfbd787cb27765f3f36aa7e3e4ef524f
Padillanur Fatmah Azzahra <padillanurfatmahazzahra@gmail.com>
2023-03-28 13:33:06 +0700

    First Commit

--- start.sh
+++ start.sh
@@ -2,12 +2,14 @@ set -eu
 
 export PYTHONUNBUFFERED=true
 
+VIRTUALENV=.data/venv
+
 if [ ! -d $VIRTUALENV ]; then
   python3 -m venv $VIRTUALENV
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --slent --show-error --retry 5 gttps://bootstrap.pypa.io/get-pip.py | $VIRTUALENV
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
