23988df53109bfc04913954d72b05daaf08fc7b2
Padillanur Fatmah Azzahra <padillanurfatmahazzahra@gmail.com>
2023-03-28 13:18:24 +0700

    add start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,12 @@ set -eu
 
 export PYTHONUNBUFFERED=true
 
-VIRTUALENV=.data/venv
-
 if [ ! -d $VIRTUALENV ]; then
   python3 -m venv $VIRTUALENV
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+  curl --slent --show-error --retry 5 gttps://bootstrap.pypa.io/get-pip.py | $VIRTUALENV
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
